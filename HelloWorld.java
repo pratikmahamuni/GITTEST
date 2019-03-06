4e86f133692764ae817ffe5ee24f11cec4d8b22c
pratikmahamuni <pratik.mahamuni@firstcry.com>
2019-03-06 15:35:26 +0530

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+Public class HelloWorld{
+  Public static void public static void main(String[] args) {
+    // prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World");
+  }
+} 
